11f8084df413465d8897b50bc67a99070245ed5b
gilanggsb <gilanggsb@gmail.com>
2022-01-17 10:01:42 +0700

    fix Profile Bloc

--- lib/bloc/profileb_bloc.dart
+++ lib/bloc/profileb_bloc.dart
@@ -9,28 +9,24 @@ part 'profileb_bloc.freezed.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
   ProfileBloc() : super(const _Initial()) {
-    on<ProfileEvent>((event, emit) async{
+    on<_getAllUser>((event, emit) async {
       //cek event pada bloc dengan event.map
       //ada 2 properti pada event yaitu started dan getAllUser
       //masing - masing properti mereturn function
-       await event.map(
-        started: (value) {},
-        getAllUser: (value) async {
-          try {
-            //emit Loading state
-           emit( const ProfileState.isLoading());
-          
-            //get data from API
-            List<UserModel> _user =
-                await ProfileService().getAllUser(page: value.page);
-            //emit Success state
-            emit(ProfileState.isSuccess(_user));
-          } catch (e) {
-            //emit Error state
-            emit(ProfileState.isError(e.toString()));
-          }
-        },
-      );
+
+      try {
+        //emit Loading state
+        emit(const ProfileState.isLoading());
+
+        //get data from API
+        List<UserModel> _user =
+            await ProfileService().getAllUser(page: event.page);
+        //emit Success state
+        emit(ProfileState.isSuccess(_user));
+      } catch (e) {
+        //emit Error state
+        emit(ProfileState.isError(e.toString()));
+      }
     });
   }
 }
